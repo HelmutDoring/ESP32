66ae20066705b12752cfa57b9cb27f3f6f330505
Helmut Döring <helmut.doring@slug.org>
2024-12-17 14:39:13 -0800

    Add files via upload
    https://www.waveshare.com/wiki/ESP32-S3-Zero

---
+++ esp32-s3-zero-led-play.py
@@ -0,0 +1,26 @@
+#
+# This is an RGB LED exerciser for the
+# ESP32 S3 Zero Development Board as
+# Found on AliExpess.
+#
+# The relative weights of the colors are
+# what seem to me to look pleasing.
+#
+# Blame: helmut.doring@slug.org
+#
+
+from machine import Pin
+from time import sleep
+from neopixel import NeoPixel
+from random import randrange
+
+pin = Pin(21, Pin.OUT) # Pin for RGB led on ESP32-S3 ZERO
+led = NeoPixel(pin, 1) # "1" = one RGB led on the "led bus"
+
+while True:
+    red = randrange(1,14,2)
+    green = randrange(1,10,2)
+    blue = randrange(1,6,2)
+    led[0] = (red, green, blue)
+    led.write()
+    sleep(0.1)
---
+++ esp32-s3-zero_install-mypishell.sh
@@ -0,0 +1,2 @@
+./rshell/r.py --port /dev/ttyACM0 cp -r ./mipyshell/bin /
+./rshell/r.py --port /dev/ttyACM0 cp -r ./mipyshell/lib /
