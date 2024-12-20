c5e9e1b2302f56fd303155037db0ea678ee6e8cf
Helmut Döring <helmut.doring@slug.org>
2024-12-20 12:16:59 -0800

    Update esp32-s3-zero_install-mypishell.sh

--- ESP32-S3-Zero/esp32-s3-zero_install-mypishell.sh
+++ ESP32-S3-Zero/esp32-s3-zero_install-mypishell.sh
@@ -1,2 +1,3 @@
-./rshell/r.py --port /dev/ttyACM0 cp -r ./mipyshell/bin /
-./rshell/r.py --port /dev/ttyACM0 cp -r ./mipyshell/lib /
+# If your root directory is not named "/pyboard/" you installed the wrong MicroPython bin file.
+./rshell/r.py --port /dev/ttyACM0 cp -r ./mipyshell/bin /pyboard/
+./rshell/r.py --port /dev/ttyACM0 cp -r ./mipyshell/lib /pyboard/
