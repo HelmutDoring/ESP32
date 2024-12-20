36cfc5ac7d0d888dd0af30abb7c1bc5680e0b7b5
Helmut Döring <helmut.doring@slug.org>
2024-12-20 12:18:08 -0800

    Add files via upload

---
+++ ESP32-S3-Zero/esp32-s3-zero_sh1106+tcs3472.py
@@ -0,0 +1,44 @@
+import sys
+
+sys.path.append("/hwlib")
+
+from time import sleep
+from machine import Pin, SoftI2C
+import sh1106  # OLED display
+from tcs3472 import tcs3472  # RGB Color Sensor
+
+
+def getrgb():
+    p0.on()  # Turn on LED's
+    SCL = 5  # Or whatever public pin you choose
+    SDA = 6  # Or whatever public pin you choose
+    bus = SoftI2C(sda=Pin(SDA), scl=Pin(SCL))
+    tcs = tcs3472(bus, 0x29)  # Default address for tcs3472
+    color = tcs.rgb()
+    sleep(1)
+    p0.off()
+    sleep(1)
+    #p0.off()  # Turn off LED's
+    return color
+
+
+def write_oled(rgb):
+    SCL = 1
+    SDA = 2
+    RST = 4
+    FREQ = 1000000
+    bus = SoftI2C(scl=Pin(SCL), sda=Pin(SDA), freq=FREQ)
+    oled = sh1106.SH1106_I2C(128, 64, bus, Pin(RST), 0x3C)
+    oled.sleep(False)
+    oled.fill(0)
+    msg = ", ".join(map(str, rgb))
+    oled.text(msg, 0, 32, 1)
+    oled.show()
+
+
+p0 = Pin(4, Pin.OUT)  # cfg pin 10 to control illumination LEDs on RGB sensor
+while True:
+    write_oled(getrgb())
+
+
+
