099071b00dd17cdf4a3b16e295307cd6aa6ac898
Helmut Döring <helmut.doring@slug.org>
2026-07-08 18:27:41 -0700

    Implement OUI lookup and CPU information reporting
    Added OUI text search functionality and CPU info display.

--- probeesp32.py
+++ probeesp32.py
@@ -2,16 +2,27 @@
 #
 # Report memory and disk usage of an esp32 board
 #
+# Now uses ouitext OUI database from wifi-scanner.py
+#
 # Blame: helmut.doring@slug.org
 #
 
-
 import esp
+import esp32
 import io
 import uos as os
 import gc
 import sys
 import machine
+
+def oui_text_search(hex):
+    oui_db = f"./ouitext/{hex[0:1]}.txt"
+    print(f"{oui_db}")
+    with open(oui_db, "r") as file:
+        for line in file:
+            if line[:8] == hex:
+                file.close()
+                return line.strip()
 
 # For the sake of consistency, the MAC address is
 # the unique i.d. in MicroPython. Note that the
@@ -20,7 +31,8 @@ import machine
 mac_addr = machine.unique_id()
 mac_addr = "{:02x}:{:02x}:{:02x}:{:02x}:{:02x}:{:02x}".format(*mac_addr)
 print(f"Unique ID: {mac_addr}")
-
+mfg = oui_text_search(mac_addr[:8].upper())
+print(f"OUI Manufacturer: {mfg}")
 print(f"pyboard: {sys.platform}")
 
 
@@ -62,6 +74,31 @@ def free():
     print(f"RAM: {used} kB used of {total} ({free} free)")
 
 
+def cpuinfo():
+    cpu_freq = machine.freq()
+    sys_info = os.uname()
+    cpu_type = sys_info.machine
+
+    if "ESP32S3" in cpu_type:
+        mcu_type = "Xtensa LX7 Dual-core"
+    elif "ESP32C3" in cpu_type:
+        mcu_type = "RISC-V Single-core"
+    elif "ESP32" in cpu_type:
+        mcu_type = "Xtensa LX6 Dual-core"
+    else:
+        mcu_type = cpu_type
+        
+    print(f"CPU Frequency: {cpu_freq / 1_000_000} MHz")
+    print(f"Chip/Platform: {mcu_type}")
+    print(f"Firmware Build: {sys_info.version}")
+    try:
+        raw = esp32.raw_temperature()
+        print(f"Internal Die Temp: {raw}°F {(raw - 32) * 5/9}°C")
+    except Exception as e:
+        raw = esp32.mcu_temperature()
+        print(f"Internal Die Temp: {raw * 9/5 + 32}°F {raw}°C")
+
+cpuinfo()
 df()
 free()
 print("\n")
